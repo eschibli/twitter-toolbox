9ef7a508047be5675dc38738d1cdb7ec2e42b819
Moe <Moe520@users.noreply.github.com>
2020-02-15 13:57:58 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# twitter-toolbox
+#UNDER CONSTRUCTION
+
+We are working on a small package that gives a one liner to clean a list of tweets, then another one liner to classify them based on sentiment
---
+++ setup.py
@@ -0,0 +1,35 @@
+import sys , os
+import setuptools # USe to build pckg
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "twitter-nlp-toolkit", # name shown on pypi and used with import
+    # All the scripts need to be inside numdata
+    version ="0.0.1",
+    author = "Dr. Eric Schibli , Mohamad (Moe) Antar",
+    author_email = "moe.antar14@gmail.com",
+    description = "Tools for collecting , processing and analyzing twitter data",
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    url="", # url of github repo
+    keywords = "package numbers calculations",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Operating System :: OS Independent" 
+    ],
+    install_requires=[
+          'numpy',
+          'pandas',
+          'sklearn.feature_extraction.text',
+          'sklearn.linear_model',
+          'keras',
+          
+      ],
+    python_requires='>=3.6',
+    include_package_data=True
+
+
+)
