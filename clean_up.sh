bf29e9c6a6af8d03703dd5dcb4d4500f6099a518
Moe <Moe520@users.noreply.github.com>
2020-02-18 17:38:12 -0800

    updated version number

--- clean_up.sh
+++ clean_up.sh
@@ -4,3 +4,13 @@ rm *.tmp
 rm *.zip
 rm *.csv
 rm *.pkl
+
+rm -rf trained_models/
+
+rm -rf twitter_nlp_toolkit.egg-info
+
+rm -rf
+
+if [ -d build ]; then rm -rf build; fi 
+
+if [ -d dist ]; then rm -rf dist; fi 
